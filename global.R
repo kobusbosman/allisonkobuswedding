48f87199792a4485a3304df4d6ef3bde5feb5a73
Kobus Bosman (KOBO) <k.bosman@rijkzwaan.nl>
2023-03-17 17:01:55 +0100

    Exclude more images

--- global.R
+++ global.R
@@ -81,7 +81,18 @@ excluded_images <- c(
   "IMG-1642_imageboard.jpg_converted.jpg",
   "IMG-4568_imageboard.jpg_converted.jpg",
   "20201102_140259_imageboard.jpg_converted.jpg",
-  "IMG-3365_imageboard.jpg_converted.jpg"
+  "IMG-3365_imageboard.jpg_converted.jpg",
+  "20200814_144606_imageboard.jpg_converted.jpg",
+  "20200815_164714_imageboard.jpg_converted.jpg",
+  "IMG-20210812-WA0002_imageboard.jpg_converted.jpg",
+  "FB_IMG_1660937795772_imageboard.jpg_converted.jpg",
+  "20201030_155544_imageboard.jpg_converted.jpg",
+  "1c38e0d8-c6d4-4206-b74c-4d8ff0ab3612_imageboard.JPG_converted.jpg",
+  "IMG-20210513-WA0012_imageboard.jpg_converted.jpg",
+  "IMG-1928_imageboard.jpg_converted.jpg",
+  "20230101_020456_imageboard.jpg_converted.jpg",
+  "IMG-0519_imageboard.jpg_converted.jpg",
+  "76aadb3b-5959-453a-9b0b-33973592c3d1_imageboard.JPG_converted.jpg"
 )
 imagepaths_read <- readLines("www/imagepaths.txt")
 
