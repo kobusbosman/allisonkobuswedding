9bb80348b23b8a1d476706e2b3c3cef1426867d3
Kobus Bosman (KOBO) <k.bosman@rijkzwaan.nl>
2023-10-04 12:07:25 +0200

    Add readr namespace to prepareqrcodes.R

--- prepareqrcodes.R
+++ prepareqrcodes.R
@@ -1,8 +1,8 @@
 if (interactive()) {
-  paymentlinks <- read_csv(
+  paymentlinks <- readr::read_csv(
     "www/paymentlinks.csv",
-    locale = locale(decimal_mark = ","),
-    col_types = cols(.default = col_character())
+    locale = readr::locale(decimal_mark = ","),
+    col_types = readr::cols(.default = readr::col_character())
   )
   apply(paymentlinks, 1, function(x) {
     png(paste0("www/", x[["tool"]], ".png"))
